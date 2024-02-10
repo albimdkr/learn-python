f4d29614365f462c2bb56cbc77d498e2e7491974
albimdkr <albimudakar04@gmail.com>
2024-02-10 18:49:48 +0700

    feat: add data type

--- intro.py
+++ intro.py
@@ -1,22 +1,32 @@
 # Variable
-greeting = 'Hello World!'
-print (greeting)
+# greeting = 'Hello World!'
+# print (greeting)
 
 # Additional
-addition = 2+2
-result = addition - 1
-print (result)
+# addition = 2+2
+# result = addition - 1
+# print (result)
 
 # Input
-name = input('Input your name: ')
-print(name)
+# name = input('Input your name: ')
+# print(name)
 
 # Data Typing (Dynamic)
-x = 5
-print(type(x)) #int
+# x = 5
+# print(type(x)) #int
 
-x = "5"
-print(type(x)) #str
+# x = "5"
+# print(type(x)) #str
+
+# Data Type
+x = 6
+print(type(x))
+
+x = 6.0
+print(type(x))
+
+x = 1+2j
+print(type(x))
 
 
 
